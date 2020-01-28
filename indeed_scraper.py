02bc516edaca1fe763bcae753e475714c3372010
Paulus Kutrich <pkutrich@gmail.com>
2020-01-27 17:44:46 -0800

    Add protection for file name entry. Clean up.

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -27,7 +27,7 @@ API_KEY = os.getenv('API_KEY')
 class IndeedScraper(object):
     """Get Indeed.com job listings that match closest with your provided document.
     
-    Use BeautifulSoup to scrape Indeed.com for job listings and descriptions.
+    Use BeautifulSoup4 to scrape Indeed.com for job listings and descriptions.
     Use the Spacy NLP library to vectorize each listing and your provided document.
     Then, use KNN to find listings most relevant to your provided document.
     Get the results right in your email inbox!
@@ -44,18 +44,17 @@ class IndeedScraper(object):
     """
 
     def __init__(self) -> None:
-        self.pages = self.num_user_input('Enter number of pages to search:\n')  # Number of indeed pages to search.
-        self.num_jobs = self.num_user_input('Enter max job listings to receive:\n') * 2  # Buffer for duplicates.
-        self.resume_path = self.user_input('Enter document file name:\n')
-        self.resume = self.load_resume(self.resume_path)
-        self.email = self.user_input('Enter email:\n')
-        print('\nYou may leave any of the following prompts blank to broaden your search.\n')
-        self.city = self.user_input('Enter city:\n').strip().title()
-        self.state = self.user_input('Enter state:\n').strip().upper()
-        self.terms = self.user_input('Enter job title:\n').strip().lower()
+        self.pages = self.num_user_input('\nEnter number of pages to search:\n')  # Number of indeed pages to search.
+        self.num_jobs = self.num_user_input('\nEnter max job listings to receive:\n') * 2  # Buffer for duplicates.
+        self.resume = self.load_resume()
+        self.email = self.user_input('\nEnter email:\n')
+        print('\nYou may leave any of the following prompts blank to broaden your search.')
+        self.city = self.user_input('\nEnter desired city:\n').strip().title()
+        self.state = self.user_input('\nEnter state abbreviation:\n').strip().upper()
+        self.terms = self.user_input('\nEnter desired job title:\n').strip().lower()
         self.url = self.build_url()
         self.http = urllib3.PoolManager()
-        print('Loading NLP packages...')
+        print('\nLoading NLP packages...')
         self.nlp = spacy.load('en_core_web_lg')
         self.nn = NearestNeighbors(n_neighbors=self.num_jobs,
                                    algorithm='ball_tree')
@@ -73,19 +72,36 @@ class IndeedScraper(object):
         self.get_best_jobs()
         self.email_jobs()
 
-    @staticmethod
-    def load_resume(path) -> str:
+    def load_resume(self) -> str:
         """Load resume text from disc."""
-        print('\nLoading document...')
-        with open(path, 'r') as f:
-            resume = f.read().strip('\n')
+        while True:
+            path = self.user_input('\nEnter document file name:\n')
+            if path[-3:] != "txt":
+                print(f'\n{"-" * 20}')
+                print('File name must end in ".txt"')
+                print(f'{"-" * 20}')
+                continue
+            try:
+                with open(path, 'r') as f:
+                    print('\nLoading document...')
+                    resume = f.read().strip('\n')
+                break
+            except FileNotFoundError:
+                print(f'\n{"-" * 20}')
+                print(f"Can't find {path}")
+                directory = os.path.curdir
+                files = [file for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
+                print(f'Files I can see: \n\n{", ".join(files)}\n')
+                print('Did you copy your file to the Docker container?')
+                print('See instructions at: https://github.com/llpk79/Job-finder/packages/111889')
+                print(f'{"-" * 20}')
         return resume
 
     def build_url(self) -> str:
         """Builds search url from user input."""
         url = f'http://www.indeed.com/jobs?q=' \
               f"{'%20'.join(self.terms.split())}&l={'%20'.join(self.city.split())},%20{self.state}"
-        print('Search URL: ', url)
+        print(f'\nSearch URL: {url}')
         return url
 
     @staticmethod
@@ -99,12 +115,10 @@ class IndeedScraper(object):
         while True:
             try:
                 num = int(input(prompt))
-                break
+                return num
             except ValueError:
-                print('Please enter a number.')
+                print('\nPlease enter a number.\n')
                 continue
-
-        return num
 
     @staticmethod
     def find_long_descriptions(soup) -> list:
@@ -123,7 +137,7 @@ class IndeedScraper(object):
 
     def get_descriptions(self) -> list:
         """Create a list of text extracted from long form job descriptions."""
-        print('Getting job descriptions...')
+        print('\nGetting job descriptions...\n')
         descriptions = []
         # Get and parse each top level page.
         for base_url in tqdm(self.get_next_pages()):
@@ -147,17 +161,17 @@ class IndeedScraper(object):
                                         attrs={'id': 'jobDescriptionText'})
                 if description:
                     descriptions.append((the_url, description.text, title.text))
-        print(f"Found {len(descriptions)} jobs.")
+        print(f"\nFound {len(descriptions)} jobs.")
         return descriptions
 
     def get_description_vectors(self) -> list:
         """Get Spacy vectors for each long form job description."""
-        print('Getting description vectors...')
+        print('\nGetting description vectors...\n')
         return [self.nlp(doc).vector for _, doc, _ in tqdm(self.descriptions)]
         
     def get_best_jobs(self) -> None:
         """Vectorize resume and fit a nearest neighbors classifier to find desired number of jobs."""
-        print(f'Finding best {self.num_jobs // 2} job matches...')
+        print(f'\nFinding best {self.num_jobs // 2} job matches...\n')
         self.nn.fit(self.vectors)
         neighbors = list(self.nn.kneighbors([self.nlp(self.resume).vector], self.num_jobs, return_distance=False)[0])
         for neighbor in neighbors:
@@ -180,7 +194,7 @@ class IndeedScraper(object):
 
     def email_jobs(self) -> None:
         """Send list of jobs to user."""
-        print('Emailing jobs...')
+        print('\nEmailing jobs...\n')
         msg = self.build_message()
         server = self.initialize_server()
         self.send_and_deactivate(server, msg)
